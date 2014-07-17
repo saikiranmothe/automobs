07d95e8b184c4df07178bb356973d39778d75801
saikiranmothe <saikiran.mothe@gmail.com>
2014-07-17 17:08:55 +0530

    updated code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Automobs::Application.config.secret_token = '46e94a6ec94924be93d4fd3e4c064b4d5f7c932399474c280a08544ac6dfec4d9f774164aa1e03b3d770e538692142664383f7165b03d31728551c4104ca75e8'
